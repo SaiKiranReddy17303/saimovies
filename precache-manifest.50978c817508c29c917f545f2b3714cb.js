3318d900148fcd99adb6e896fd2310e4fd72e42e
bunnyreddy17303 <bunnyreddy830@gmail.com>
2020-06-29 17:55:44 +0530

    Updates

---
+++ precache-manifest.50978c817508c29c917f545f2b3714cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cdff223352389996e9e845b00abbccc",
+    "url": "/bunnyreddy17303/saimovies/index.html"
+  },
+  {
+    "revision": "819ba7cbc31bee6f64fd",
+    "url": "/bunnyreddy17303/saimovies/static/css/main.57fbc47d.chunk.css"
+  },
+  {
+    "revision": "78ca6dd46f4f74e224a7",
+    "url": "/bunnyreddy17303/saimovies/static/js/2.a13bd1df.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bunnyreddy17303/saimovies/static/js/2.a13bd1df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "819ba7cbc31bee6f64fd",
+    "url": "/bunnyreddy17303/saimovies/static/js/main.f3309e15.chunk.js"
+  },
+  {
+    "revision": "a3fbd8f4af87fc3cf536",
+    "url": "/bunnyreddy17303/saimovies/static/js/runtime-main.0033483c.js"
+  }
+]);
